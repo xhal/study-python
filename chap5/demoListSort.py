a9375aad86aaba967583f1c9b4e9da26551be22f
hal <hal@xhal.net>
2021-02-02 21:52:40 +0800

    [优化] - 代码格式化

--- chap5/demoListSort.py
+++ chap5/demoListSort.py
@@ -5,7 +5,7 @@
 # 文件版本: V1.0.0
 # 功能描述: 列表对象 - 排序
 
-lst = [1,3,4,5,33,2,22,11,55,66,42]
+lst = [1, 3, 4, 5, 33, 2, 22, 11, 55, 66, 42]
 print(id(lst), lst)
 # 调用默认 sort(),默认为升序
 lst.sort()
@@ -16,19 +16,16 @@ print(id(lst), lst)
 lst.sort(reverse=True)
 print(id(lst), lst)
 
-
 print('--------------华丽的分隔线-----------------')
 # 内置函数 sorted()
-lst = [1,3,4,5,33,2,22,11,55,66,42]
+lst = [1, 3, 4, 5, 33, 2, 22, 11, 55, 66, 42]
 print(id(lst), lst)
 
 nlst = sorted(lst)
 
-print(id(lst), lst) # 原列表不变
+print(id(lst), lst)  # 原列表不变
 print(id(nlst), nlst)
 
 nlst = sorted(lst, reverse=True)
-print(id(lst), lst) # 原列表不变
+print(id(lst), lst)  # 原列表不变
 print(id(nlst), nlst)
-
-
